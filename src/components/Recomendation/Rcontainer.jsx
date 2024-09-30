47b53d27cf4de543f76a61e5c4a4a21b1c6f8944
Nihit Jain <nihit.jain@qsstechnosoft.com>
2024-09-30 14:33:37 +0530

    test

--- src/components/Recomendation/Rcontainer.jsx
+++ src/components/Recomendation/Rcontainer.jsx
@@ -1,22 +1,22 @@
-import React from "react";
-import MyCard from "./MyCard";
+// import React from "react";
+// import MyCard from "./MyCard";
 
-function Rcontainer() {
-  return (
-    <>
-      <h1 className="text-3xl text-gray-900 font-sans mt-[60px]">
-        Today's recommendations for you, Charlie!
-      </h1>
+// function Rcontainer() {
+//   return (
+//     <>
+//       <h1 className="text-3xl text-gray-900 font-sans mt-[60px]">
+//         Today's recommendations for you, Charlie!
+//       </h1>
 
-      <div className="flex gap-3">
-        <MyCard url="/images/spa_0.png" index="/images/one.svg" />
-        <MyCard url="/images/holiday_0.png" index="/images/two.svg" />
-        <MyCard url="/images/cusine_0.png" index="/images/three.svg" />
-        <MyCard url="/images/rolce.png" index="/images/four.svg" />
-        <MyCard url="/images/Event-Banner.png" index="/images/five.svg" />
-      </div>
-    </>
-  );
-}
+//       <div className="flex gap-3">
+//         <MyCard url="/images/spa_0.png" index="/images/one.svg" />
+//         <MyCard url="/images/holiday_0.png" index="/images/two.svg" />
+//         <MyCard url="/images/cusine_0.png" index="/images/three.svg" />
+//         <MyCard url="/images/rolce.png" index="/images/four.svg" />
+//         <MyCard url="/images/Event-Banner.png" index="/images/five.svg" />
+//       </div>
+//     </>
+//   );
+// }
 
-export default Rcontainer;
+// export default Rcontainer;
